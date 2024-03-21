15c92826c0886147ee4d12d3384acf01df4183ab
Kursat Korkmaz <66425950+kursattkorkmazzz@users.noreply.github.com>
2024-03-21 11:38:35 +0300

    Project Page

---
+++ project/js/script/imagePreview.js
@@ -0,0 +1,15 @@
+const imageUpload = document.getElementById('imageUpload');
+const previewContainer = document.getElementById('previewContainer');
+
+imageUpload.addEventListener('change', function() {
+  const reader = new FileReader();
+  reader.onload = function (e) {
+    const img = document.createElement('img'); // Create an image element
+    img.src = e.target.result;
+    img.alt = "Selected Image Preview";
+    img.classList= "image-preview"
+    previewContainer.innerHTML = ""; // Clear previous preview (if any)
+    previewContainer.appendChild(img); // Add the image to the preview container
+  };
+  reader.readAsDataURL(this.files[0]);
+});
