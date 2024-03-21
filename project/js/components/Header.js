a57ce48d34f57beb7f15596f8f6d5de0cca980dc
Kursat Korkmaz <66425950+kursattkorkmazzz@users.noreply.github.com>
2024-03-21 11:46:34 +0300

    Add files via upload

---
+++ project/js/components/Header.js
@@ -0,0 +1,31 @@
+// This class is HTML element and represents our header element.
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+    <header id="header">
+      <div id="banner">
+        <h1>Product Name</h1>
+      </div>
+      <nav id="navbar">
+        <ul>
+          <li>
+            <a href="./">Homepage</a>
+          </li>
+          <li>
+            <a href="./dashboard.html">Go to app</a>
+          </li>
+        </ul>
+        <li>
+          <a href="./login.html">Login or Register</a>
+        </li>
+      </nav>
+    </header>
+    `;
+  }
+}
+
+customElements.define("common-header", Header);
---
+++ project/js/components/ProjectCard.js
@@ -0,0 +1,22 @@
+class ProjectCard extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.innerHTML = `
+    <div id="card-wrapper">
+    <img
+      id="card-thumbnail"
+      src="https://dummyimage.com/600x300/525252/fff.png"
+    />
+    <div id="card-bottom">
+      <h2 id="card-bottom-header">Project Title</h2>
+      <button class="bg-blue text-white" onclick="window.location.assign('./project.html')">Go to Project</button>
+    </div>
+  </div>
+    
+    `;
+  }
+}
+
+customElements.define("project-card", ProjectCard);
